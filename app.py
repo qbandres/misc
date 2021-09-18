335e3776c72491efd0b8ff0579fb6ec282ecc036
qbandreswn <qbandres@gmail.com>
2021-09-18 00:04:01 -0300

    Se incluye insert windows

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 a = input('Ingrese su nombre ')
 b = input('Ingrese su apellido ' )
 c = input("Ingrese su sistema operativo para mac ")
-
-print(f'Hello world {a} {b}, su Mac Opera con {c}')
+d = input('Ingrese su sistema operativo de Windows ')
+print(f'Hello world {a} {b}, su Mac Opera con {c} y el windows con {d}')
 
